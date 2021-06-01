6d67440893fb4e06f4417670b34e1ff8400b5125
Tobias Koppers <tobias.koppers@googlemail.com>
2021-06-01 09:23:53 +0200

    parallel requests

--- lib/search-props.tsx
+++ lib/search-props.tsx
@@ -8,8 +8,10 @@ export async function getSearchStaticProps({
   locales,
 }: GetStaticPropsContext) {
   const config = { locale, locales }
-  const { pages } = await commerce.getAllPages({ config, preview })
-  const { categories, brands } = await commerce.getSiteInfo({ config, preview })
+  const pagesPromise = commerce.getAllPages({ config, preview })
+  const siteInfoPromise = commerce.getSiteInfo({ config, preview })
+  const { pages } = await pagesPromise
+  const { categories, brands } = await siteInfoPromise
   return {
     props: {
       pages,
